11912713ab789c06ac4e642019a5ed0b04429c63
Jonathan <jonathanmayceh@gmail.com>
2020-08-10 15:06:23 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+//const port = 3000
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', (req, res) => {
+    res.json('Get Usuario!')
+});
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            body
+        });
+
+    }
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('Delete Usuario')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+})
